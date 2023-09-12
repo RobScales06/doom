7b27b1038d8873fcb13d93514cb9f81ea9074092
RobScales06 <56702816+RobScales06@users.noreply.github.com>
2023-09-11 20:48:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 // }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 31, 2021 00:00:00").getTime();
+var countDownDate = new Date("Oct 31, 2023 00:00:00").getTime();
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
